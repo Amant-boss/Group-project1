ac4f67b190c0dc51e58fa1e39e5b4201be645d21
DrGashi <drengashids@gmail.com>
2025-04-30 09:43:21 -0700

    Simplifyed smth

--- js.js
+++ js.js
@@ -37,12 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    const getStartedButton = document.querySelector('header .cta-button');
-    getStartedButton.addEventListener('click', function() {
-        document.querySelector('#anchor').scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
 
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
